7fdaee9481c6f200d605064aa50bee4cad9b7e29
Kris Pinter <pinter.krisztian89@gmail.com>
2016-10-22 10:32:56 +0100

    phase 1 css html js done

---
+++ js/game.js
@@ -0,0 +1,62 @@
+var colors = generateRandomColors(6);
+var pickedColor = pickColor();
+var messageDisplay = document.querySelector("#message");
+var colordisplay = document.getElementById('colordisplay');
+colordisplay.textContent = pickedColor;
+var squares = document.querySelectorAll('.square');
+var header = document.querySelector('.header');
+var resetButton = document.querySelector('.controls .reset');
+
+resetButton.addEventListener("click", function(){
+  colors = generateRandomColors(6);
+  resetButton.textContent = "New Colors"
+  for (var i=0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  }
+  pickedColor = pickColor();
+  colordisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
+  header.style.background = "#232323";
+});
+
+for (var i=0; i < squares.length; i++) {
+  squares[i].style.background = colors[i];
+  squares[i].addEventListener("click", function() {
+    var clickedColor = this.style.background
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct";
+      changeColors(pickedColor)
+      header.style.background = pickedColor;
+      resetButton.textContent = "Play again?"
+    } else {
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try again!";
+    }
+  });
+}
+
+function changeColors(color) {
+  for (var i=0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+  for (var i=0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" +r+ ", " +g+ ", "+b+ ")"
+}
